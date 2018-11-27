f0c50a7812aea6eacd04aa7e29e118f3536c1c20
David Flores <dflores@farmaciasperuanas.pe>
2018-11-27 09:49:56 -0500

    Agregando conexión a la nube de la Web RestPoliticas

--- www/public/modelos/conexion.php
+++ www/public/modelos/conexion.php
@@ -4,11 +4,27 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;
-						dbname=restpoliticas",
-						"root",
-						""
-						);
+/*======================================
+=            CONEXION LOCAL            =
+======================================*/
+/*	$link = new PDO("mysql:host=localhost;
+					dbname=restpoliticas",
+					"root",
+					""
+					);*/
+/*=====  End of CONEXION LOCAL  ======*/
+
+/*======================================
+=            CONEXION CLOUD            =
+======================================*/
+	$link = new PDO("mysql:host=3.16.245.0;
+					dbname=restpoliticas",
+					"root",
+					"caba123lleria"
+					);
+/*=====  End of CONEXION CLOUD  ======*/
+
+
 
 		$link->exec("set names utf8");
 
